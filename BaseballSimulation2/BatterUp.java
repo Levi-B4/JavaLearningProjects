e43aa3ac5f1fd1924c15ef6c9e02d6ed70b876f6
Levi-B4 <levi.butler4@gmail.com>
2023-07-06 14:59:29 -0400

    Updated BatterUp and Player classes in BaseballSimulation2

--- BaseballSimulation2/BatterUp.java
+++ BaseballSimulation2/BatterUp.java
@@ -1,18 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BatterUp {
     private int outs;
     private int score;
     private int nextPlayerIndex;
+    private ArrayList<Player> players;
+    private Field field;
 
     public BatterUp() {
-        
+        outs = 0;
+        score = 0;
+        nextPlayerIndex = 0;
+        CreatePlayers();
+        field = new Field();
     }
 
     public void CreatePlayers(){
-
+        Base dugout = field.getDugout();
+        players = new ArrayList<Player>(Arrays.asList(
+            new Player("Larry", dugout),
+            new Player("Landon", dugout),
+            new Player("Levi", dugout),
+            new Player("Lionel", dugout),
+            new Player("Liam", dugout),
+            new Player("Lucy", dugout),
+            new Player("Lucas", dugout),
+            new Player("Logan", dugout),
+            new Player("Lincoln", dugout)
+        ));
     }
 
     public Player getNextPlayer(){
-        return new Player("pLACEHOLDER", new Base("PLACEHOLDER"));
+        Player nextPlayer = players.get(nextPlayerIndex);
+        nextPlayerIndex += 1;
+        if(nextPlayerIndex > players.size()){
+            nextPlayerIndex = 0;
+        }
+        return nextPlayer;
     }
 
     public void Play(){
--- BaseballSimulation2/Player.java
+++ BaseballSimulation2/Player.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Player {
     
     private String name;
@@ -12,31 +14,64 @@ public class Player {
     }
 
     public String getName(){
-        return "PLACEHOLDER";
+        return name;
     }
 
     public void setName(String name){
-
+        this.name = name;
     }
 
     public Base getLocation(){
-        return new Base("PLACEHOLDER");
+        return location;
     }
 
-    public void setLocation(Base base){
-        
+    public void setLocation(Base location){
+        this.location = location;
     }
 
     public boolean isNotInDugout(){
-        return true;
+        return location.isDugout();
     }
 
     public int takeTurn(){
-        return 0;
+        strikes = 0;
+        balls = 0;
+        int batterBatResult;
+
+        while(strikes < 3 && balls < 4){
+            batterBatResult = bat();
+            if(batterBatResult > 0){
+                return batterBatResult;
+            }
+        }
+    
+        //walk or strikeout
+        if(balls == 4){
+            return 1;
+        } else{
+            return 0;
+        }
     }
 
     public int bat(){
-        return 0;
+        Random rand = new Random();
+        //simulate two dice rolls
+        int dice1 = rand.nextInt(6) + 1;
+        int dice2 = rand.nextInt(6) + 1;
+        
+        System.out.printf("Roll: %d", dice1);
+        System.out.printf(" %d    ", dice2);
+        
+        if(dice1 == dice2 && dice1 <= 4){
+            return dice1;
+        } else{
+            if((dice1 + dice2 )% 2 == 0){
+                strikes =+ 1;
+            } else{
+                balls += 1;
+            }
+            return 0;
+        }
     }
 
     public String toString(){
